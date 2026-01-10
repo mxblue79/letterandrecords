650fd567161d6bea28ea791bd38e35a6b70a04d1
letterandrecords <shinjaelee79@gmail.com>
2026-01-10 20:35:53 +0900

    Add appId to sanity cli config

--- my-portfolio/sanity.cli.ts
+++ my-portfolio/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
+    appId: 'cetoq4ie4oyudwyazfx6ugkj',
   }
 })
